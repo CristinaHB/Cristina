b3082451523d75c534963961c44ad6121a323d12
CristinaHB <kriistina1996@gmail.com>
2017-03-10 14:18:53 +0100

    Añadida linea

--- src/cristina1/Cristina1.java
+++ src/cristina1/Cristina1.java
@@ -7,6 +7,7 @@ public class Cristina1 {
    
     public static void main(String[] args) {
        System.out.println("Inicio de proyecto");
+       System.out.println("Inicio de proyecto 2");
     }
     
 }
